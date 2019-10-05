4e95a34c98d6dc884b6abc5e895e95152683c53f
Vipin Bailwal <vipinbailwal05@gmail.com>
2019-10-05 21:49:21 +0530

    added functionality to capture given number of screenshots after a delay

--- src/snappyscreenshots/Main.java
+++ src/snappyscreenshots/Main.java
@@ -2,7 +2,7 @@ package snappyscreenshots;
 import java.awt.*;
 import javax.imageio.ImageIO;
 import java.io.File;
-
+import java.util.Scanner;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -10,11 +10,24 @@ public class Main {
     public static void main(String[] args) throws AWTException, IOException {
         Robot robot = new Robot(); // create a robot to take screenshot
         Rectangle screen = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()); // define the area to capture (the current screen)
-        BufferedImage image = robot.createScreenCapture(screen);
+
         // This will store screenshot on Specific location
         String outputDirectory = "/home/vb/Pictures/snappy-screenshots/";
-        new File(outputDirectory).mkdir(); // creating the output directory to save the captured screenshot
-        ImageIO.write(image, "png", new File(outputDirectory + "testImage.png")); // write the file
-        System.out.println("Screen Captured Successfully. File saved to " + outputDirectory);
+        new File(outputDirectory).mkdir();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of images to capture : ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+
+            BufferedImage image = robot.createScreenCapture(screen);
+            // This will store screenshot on Specific location
+            ImageIO.write(image, "png", new File(outputDirectory + "testImage" + i +".png"));
+            System.out.println("Captured image : testImage"+i);
+            robot.delay(2000); // capture screenshot after every 2 seconds from starting till 'n' images have been captured
+        }
+        System.out.println("Screen Captured Successfully. All files saved to " + outputDirectory);
+
     }
 }
